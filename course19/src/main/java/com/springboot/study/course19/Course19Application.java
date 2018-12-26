87b1191378989b8cb6597f7540ae78340b003895
h1261 <h1261109615@qq.com>
2018-12-26 18:02:44 +0800

    课程18  微服务实战，springboot整合mybatis

---
+++ course19/src/main/java/com/springboot/study/course19/Course19Application.java
@@ -0,0 +1,14 @@
+package com.springboot.study.course19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course19Application.class, args);
+    }
+
+}
+
